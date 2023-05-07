8451db56787d4b7b7e079c187f6461e4494c9bf6
Yan-Santana <yansantana63@gmail.com>
2023-05-06 23:07:40 -0400

    feat: new generic button

---
+++ src/components/GenericButton.java
@@ -0,0 +1,29 @@
+package src.components;
+
+import java.io.IOException;
+
+import java.awt.FontFormatException;
+import java.awt.Insets;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JButton;
+
+public class GenericButton extends JButton {
+	private final Color mainColor;
+	private final Color whiteColor;
+    private final Font mainFont;
+
+    public GenericButton(String label) throws FontFormatException, IOException {
+		super(label);
+
+        mainColor = new Color(101, 88, 245);
+        whiteColor = new Color(255, 255, 255);
+        mainFont = new Assets().getMainFont();
+
+		this.setMargin(new Insets(6, 16, 6, 16));
+		this.setBackground(this.mainColor);
+		this.setForeground(this.whiteColor);
+		this.setFont(this.mainFont.deriveFont(Font.BOLD, 19));
+    }
+}
