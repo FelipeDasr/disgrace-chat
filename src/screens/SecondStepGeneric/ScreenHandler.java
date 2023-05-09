4b6eefabad8e5bff9163e5d3dd2c7ab805707b58
FelipeDasr <felipe.heiker07@gmail.com>
2023-05-09 16:20:53 -0300

    feat: initial structure of screen handlers

---
+++ src/screens/SecondStepGeneric/ScreenHandler.java
@@ -0,0 +1,9 @@
+package src.screens.SecondStepGeneric;
+
+public class ScreenHandler {
+    private final SecondStepGenericScreen frame;
+
+    public ScreenHandler(SecondStepGenericScreen frame) {
+        this.frame = frame;
+    }
+}
---
+++ src/screens/ServerCreation/ScreenHandler.java
@@ -0,0 +1,10 @@
+package src.screens.ServerCreation;
+
+// private class (only to be used by ServerCreationScreen.java)
+class ScreenHandler {
+    private final ServerCreationScreen frame;
+
+    public ScreenHandler(ServerCreationScreen frame) {
+        this.frame = frame;
+    }
+}
