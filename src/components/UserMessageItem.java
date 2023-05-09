1215acb4b89f8aa498d6ccb845c53bc5dfa5a0c4
FelipeDasr <felipe.heiker07@gmail.com>
2023-05-09 02:56:17 -0300

    feat: new component (UserMessageItem)

---
+++ src/components/UserMessageItem.java
@@ -0,0 +1,63 @@
+package src.components;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.IOException;
+
+import javax.swing.BorderFactory;
+import javax.swing.JTextArea;
+
+import src.entities.UserMessage;
+
+class UserMessageItem extends JTextArea {
+    private final UserMessage userMessage;
+
+    public UserMessageItem(UserMessage userMessage) throws FontFormatException, IOException {
+        this.userMessage = userMessage;
+
+        this.setFont(new Assets().getMainFont().deriveFont(Font.BOLD, 19));
+        this.setText(this.getFormatedContent());
+        this.setForeground(this.getColor());
+
+        this.setBorder(BorderFactory.createEmptyBorder(0, 10, 	0, 10));
+
+        this.setLineWrap(true);
+        this.setWrapStyleWord(true);
+        this.setEditable(false);
+    }
+
+    private String getFormatedContent() {
+        return "[" + this.userMessage.getFormatedHourString() + "h - " 
+                + this.userMessage.getUser().getName() + "]: " 
+                    + this.userMessage.getContent();
+    }
+
+    private Color getColor() {
+        switch(this.userMessage.getUser().getAvatarId()) {
+            case 0:
+                return new Color(236, 38, 143);
+
+            case 1:
+                return new Color(0, 152, 218);	
+
+            case 2:
+                return new Color(203, 115, 113);
+
+            case 3:
+                return new Color(161, 123, 182);
+
+            case 5:
+                return new Color(0, 168, 89);
+
+            case 6:
+                return new Color(96, 95, 84);
+
+            case 7:
+                return new Color(237, 47, 89);
+
+            default:
+                return new Color(32, 30, 30);
+        }
+    }
+}
