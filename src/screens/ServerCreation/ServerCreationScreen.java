461e7e8e261de16b064799aaeb087431ed029b5f
Felipe Dasr <felipe.heiker07@gmail.com>
2023-05-09 22:59:55 -0300

    feat: add the button action (confirm server creation)

--- src/screens/ServerCreation/ServerCreationScreen.java
+++ src/screens/ServerCreation/ServerCreationScreen.java
@@ -6,11 +6,16 @@ import java.io.IOException;
 import src.screens.SecondStepGeneric.SecondStepGenericScreen;
 
 public class ServerCreationScreen extends SecondStepGenericScreen {
+    private ScreenHandler screenHandler;
+
     public ServerCreationScreen() throws FontFormatException, IOException {
         super("Criar servidor");
+        this.screenHandler = new ScreenHandler(this);
 
         this.setFirstFormInputLabel("Nome:            ");
         this.setSecondFormInputLabel("Porta:              ");
         this.setButtonLabel("Criar servidor");
+
+        this.getMainButton().addActionListener(this.screenHandler.confirmServerCreationOnClick());
     }
 }
