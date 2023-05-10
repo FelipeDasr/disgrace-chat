76d22636bbd54b732657c6f7483baec5de8e58ea
FelipeDasr <felipe.heiker07@gmail.com>
2023-05-10 18:29:35 -0300

    feat: chat server methods implementation (connectToServer and disconnectFromServer)

--- src/infrastructure/client/ChatClient.java
+++ src/infrastructure/client/ChatClient.java
@@ -1,9 +1,28 @@
 package src.infrastructure.client;
 
-public class ChatClient {
-    private String name;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import src.entities.User;
+
+public class ChatClient extends User {
+    private InetAddress serverAddress;
+    private Socket socket;
     
-    public ChatClient(String name) {
-        this.name = name;
+    public ChatClient(String name, int avatarId) {
+        super(name, avatarId);
+        this.socket = new Socket();
+    }
+
+    public void connectToServer(String serverHost, int serverPort) throws IOException {
+        this.serverAddress = InetAddress.getByName(serverHost);
+        InetSocketAddress addressToConnect = new InetSocketAddress(this.serverAddress, serverPort);
+        this.socket.connect(addressToConnect);
+    }
+
+    public void disconnectFromServer() throws IOException {
+        this.socket.close();
     }
 }
