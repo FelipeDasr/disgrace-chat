7d5e95b68a86b4b484cdda26e0e401093fc2839b
Felipe Dasr <felipe.heiker07@gmail.com>
2023-05-09 22:58:00 -0300

    feat: initial structure of confirmServerCreationOnClick event

--- src/screens/ServerCreation/ScreenHandler.java
+++ src/screens/ServerCreation/ScreenHandler.java
@@ -1,5 +1,7 @@
 package src.screens.ServerCreation;
 
+import java.awt.event.ActionListener;
+
 // private class (only to be used by ServerCreationScreen.java)
 class ScreenHandler {
     private final ServerCreationScreen frame;
@@ -7,4 +9,13 @@ class ScreenHandler {
     public ScreenHandler(ServerCreationScreen frame) {
         this.frame = frame;
     }
+
+    public ActionListener confirmServerCreationOnClick() {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent event) {
+                System.out.println("Server creation confirmed");
+            }
+        };
+    };
 }
