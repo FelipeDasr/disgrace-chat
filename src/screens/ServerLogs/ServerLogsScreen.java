bc726fd42a84e712a7beea288f6c1bc236b1b371
FelipeDasr <felipe.heiker07@gmail.com>
2023-05-12 01:51:47 -0300

    feat: define panels grid

--- src/screens/ServerLogs/ServerLogsScreen.java
+++ src/screens/ServerLogs/ServerLogsScreen.java
@@ -1,12 +1,14 @@
 package src.screens.ServerLogs;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import java.awt.Dimension;
-import java.io.IOException;
 import java.awt.Color;
+
+import java.io.IOException;
 
 import src.components.Assets;
 import src.infrastructure.server.ChatServer;
@@ -17,18 +19,26 @@ public class ServerLogsScreen {
 
     public ServerLogsScreen(ChatServer server) {
         this.server = server;
+        int frameHeight = 608;
+        int frameWidth = 853;
 
         this.frame = new JFrame("Disgrace - " + this.server.getName());
 		this.frame.setLayout(new BoxLayout(this.frame.getContentPane(), BoxLayout.X_AXIS));
 		frame.setIconImage(new Assets().getAppIcon());
 
-		JPanel panel = new JPanel();
-        panel.setBackground(new Color(255, 255, 255));
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		panel.setMaximumSize(new Dimension(743, 558));
+        Color commonBorderColor = new Color(195, 207, 217);
 
-        this.frame.add(panel);
-		this.frame.setSize(743, 558);
+        JPanel leftPanel = new JPanel();
+        leftPanel.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 2, commonBorderColor));
+        leftPanel.setMaximumSize(new Dimension(250, frameHeight));
+
+        JPanel rightPanel = new JPanel();
+        rightPanel.setMaximumSize(new Dimension(frameWidth - 250, frameHeight));
+
+        frame.add(leftPanel);
+        frame.add(rightPanel);
+
+		this.frame.setSize(frameWidth, frameHeight);
 		this.frame.setLocationRelativeTo(null);
 		this.frame.setResizable(false);
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
