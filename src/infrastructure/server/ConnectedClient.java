758702c6bbdd2a94a7c34953853031063466f122
FelipeDasr <felipe.heiker07@gmail.com>
2023-05-12 10:14:48 -0300

    feat: initial structure of ConnectedClient class

---
+++ src/infrastructure/server/ConnectedClient.java
@@ -0,0 +1,33 @@
+package src.infrastructure.server;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+
+import java.net.Socket;
+
+import org.json.JSONObject;
+
+import src.entities.Client;
+
+public class ConnectedClient extends Client {
+    private final ObjectOutputStream outputChannel;
+    private final ObjectInputStream inputChannel;
+    private final Socket socket;
+
+    public ConnectedClient(String name, int avatarId, Socket socket) throws IOException {
+        super(name, avatarId);
+        this.socket = socket;
+        this.outputChannel = new ObjectOutputStream(this.socket.getOutputStream());
+        this.inputChannel = new ObjectInputStream(this.socket.getInputStream());
+    }
+
+    public void emmitEvent(String event, JSONObject data) throws IOException {
+        String dataString = data.toString();
+        this.outputChannel.writeObject(dataString);
+    }
+
+    public void closeConnection() throws IOException {
+        this.socket.close();
+    }
+}
