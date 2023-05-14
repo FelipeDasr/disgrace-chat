ab390c19573b7f3708d05e272a2b22725e6c3998
FelipeDasr <felipe.heiker07@gmail.com>
2023-05-14 14:58:40 -0300

    feat: new error declaration

--- src/interfaces/InputEventHandler.java
+++ src/interfaces/InputEventHandler.java
@@ -1,9 +1,11 @@
 package src.interfaces;
 
+import java.io.IOException;
+
 import org.json.JSONObject;
 
 import src.infrastructure.server.ConnectedClient;
 
 public interface InputEventHandler {
-    void execute(ConnectedClient connectedClient, String event, JSONObject data);
+    void execute(ConnectedClient connectedClient, String event, JSONObject data) throws IOException;
 }
