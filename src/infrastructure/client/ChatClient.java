53ff0e7e314d9adf064a701e5e39e5ec398dd075
FelipeDasr <felipe.heiker07@gmail.com>
2023-05-14 01:36:23 -0300

    feat: new method (emitEvent)

--- src/infrastructure/client/ChatClient.java
+++ src/infrastructure/client/ChatClient.java
@@ -7,6 +7,8 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
+import org.json.JSONObject;
+
 import src.entities.Client;
 
 public class ChatClient extends Client {
@@ -24,6 +26,17 @@ public class ChatClient extends Client {
         this.serverAddress = InetAddress.getByName(serverHost);
         InetSocketAddress addressToConnect = new InetSocketAddress(this.serverAddress, serverPort);
         this.socket.connect(addressToConnect);
+
+        this.inputChannel = new ObjectInputStream(this.socket.getInputStream());
+        this.outputChannel = new ObjectOutputStream(this.socket.getOutputStream());
+    }
+
+    public void emitEvent(String event, JSONObject data) throws IOException {
+        JSONObject eventObject = new JSONObject();
+        eventObject.put("event", event);
+        eventObject.put("data", data);
+
+        this.outputChannel.writeObject(eventObject.toString());
     }
 
     public void disconnectFromServer() throws IOException {
