ed6498edb24527579d28827232dea7c78c534750
FelipeDasr <felipe.heiker07@gmail.com>
2023-05-20 03:31:18 -0300

    feat: link to connected point and set the events handler

--- src/infrastructure/client/ChatClient.java
+++ src/infrastructure/client/ChatClient.java
@@ -10,18 +10,30 @@ import src.entities.Client;
 public class ChatClient extends Client {
     private InetAddress serverAddress;
     private Socket socket;
+    private ConnectedServer connectedPoint;
+
+    private final ChatClientHandler handler;
     
     public ChatClient() {
         this.socket = new Socket();
+        this.handler = new ChatClientHandler();
     }
 
     public void connectToServer(String serverHost, int serverPort) throws IOException {
         this.serverAddress = InetAddress.getByName(serverHost);
         InetSocketAddress addressToConnect = new InetSocketAddress(this.serverAddress, serverPort);
         this.socket.connect(addressToConnect);
+
+        this.createConnectedPoint();
     }
 
     public void disconnectFromServer() throws IOException {
         this.socket.close();
+    }
+
+    private void createConnectedPoint() throws IOException {
+        this.connectedPoint = new ConnectedServer(this.socket);
+        this.connectedPoint.setEventHandler(this.handler.handleEventOnReceive());
+        this.connectedPoint.listenEventsInParallel();
     }
 }
