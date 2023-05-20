668f8632205ce76ced3db72f8e188de9764e458d
FelipeDasr <felipe.heiker07@gmail.com>
2023-05-20 02:14:39 -0300

    refactor: rename class (from InputEventsListener to ConnectedPoint)

--- src/interfaces/ConnectedPoint.java
+++ src/interfaces/ConnectedPoint.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 import org.json.JSONObject;
 
-public interface InputEventListener {
+public interface ConnectedPoint {
     public void setEventHandler(InputEventHandler inputEventHandler);
     public void emitEvent(String event, JSONObject data) throws IOException;
     public void listenEventsInParallel();
