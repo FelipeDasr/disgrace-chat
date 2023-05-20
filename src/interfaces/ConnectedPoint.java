446011ef995805c9745af693a877bacb2b5e9f66
FelipeDasr <felipe.heiker07@gmail.com>
2023-05-20 02:42:25 -0300

    refactor: accept generic types (ConnectedPoint)

--- src/interfaces/ConnectedPoint.java
+++ src/interfaces/ConnectedPoint.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import org.json.JSONObject;
 
-public interface ConnectedPoint {
-    public void setEventHandler(InputEventHandler inputEventHandler);
+public interface ConnectedPoint<GenericPoint> {
+    public void setEventHandler(InputEventHandler<GenericPoint> inputEventHandler);
     public void emitEvent(String event, JSONObject data) throws IOException;
     public void listenEventsInParallel();
     public void closeConnection() throws IOException;
