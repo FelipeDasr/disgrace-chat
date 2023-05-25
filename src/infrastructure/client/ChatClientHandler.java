da6e6c068910c0522878737d3ae24b929f2aae11
FelipeDasr <felipe.heiker07@gmail.com>
2023-05-25 16:29:48 -0300

    feat: initial structure of method (joinedEvent)

--- src/infrastructure/client/ChatClientHandler.java
+++ src/infrastructure/client/ChatClientHandler.java
@@ -2,10 +2,13 @@ package src.infrastructure.client;
 
 import java.io.IOException;
 
+import java.util.Vector;
 import org.json.JSONObject;
 
 import src.interfaces.InputEventHandler;
 import src.interfaces.PointHandler;
+
+import src.entities.Client;
 
 public class ChatClientHandler implements PointHandler<ConnectedServer>{
     private ChatClient chatClient;
@@ -24,9 +27,28 @@ public class ChatClientHandler implements PointHandler<ConnectedServer>{
                         break;
                 }
             }
-
-            private void joinedEvent(ConnectedServer connectedClient, JSONObject data) {
-            }
         };
+    }
+
+    private void joinedEvent(ConnectedServer connectedClient, JSONObject data) {
+        JSONObject serverData = data.getJSONObject("server");
+        String serverName = serverData.getString("name");
+
+        int channelId = data.getInt("channelId");
+        Vector<Client> serverMembers = new Vector<Client>();
+
+        // Define all connected server members
+        for (Object memberItem: serverData.getJSONArray("connectedClients").toList()) {
+            int memberChannelId = ((JSONObject) memberItem).getInt("channelId");
+            String memberName = ((JSONObject) memberItem).getString("name");
+            int avatarId = ((JSONObject) memberItem).getInt("avatarId");
+
+            Client newMember = new Client(memberName, avatarId, memberChannelId);
+            serverMembers.add(newMember);
+        }
+
+        this.chatClient.setServerMembers(serverMembers);
+        this.chatClient.setServerName(serverName);
+        this.chatClient.setChannelId(channelId);
     }
 }
