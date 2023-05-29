3f6530c93a464b8e3f62728a8db7df0b5f757399
FelipeDasr <felipe.heiker07@gmail.com>
2023-05-28 22:34:23 -0300

    feat: initial structure of ScreenHandler (ScreenHandler.joinedMember)

--- src/screens/ServerLogs/ScreenHandler.java
+++ src/screens/ServerLogs/ScreenHandler.java
@@ -1,5 +1,26 @@
 package src.screens.ServerLogs;
 
+import java.awt.FontFormatException;
+import java.io.IOException;
+import java.util.Date;
+
+import src.components.UserConversationItem;
+import src.components.UserMessageItem;
+import src.entities.Client;
+import src.entities.ClientMessage;
+
 public class ScreenHandler {
-    
+    private ServerLogsScreen screen;
+
+    public ScreenHandler(ServerLogsScreen screen) {
+        this.screen = screen;
+    }
+
+    public void joinedMember(Client member) throws FontFormatException, IOException {
+        UserConversationItem memberItem = new UserConversationItem(member);
+        ClientMessage clientMessage = new ClientMessage(member, "Se conectou", new Date());
+
+        this.screen.getLogsPanel().add(new UserMessageItem(clientMessage), 0);
+        this.screen.getMembersPanel().add(memberItem);
+    }
 }
