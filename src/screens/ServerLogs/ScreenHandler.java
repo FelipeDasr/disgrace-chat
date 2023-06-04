870eb69018c7d0de39249cd7a3efddd6d9bc30e7
Felipe Dasr <felipe.heiker07@gmail.com>
2023-06-04 01:39:23 -0300

    feat: add a space between items

--- src/screens/ServerLogs/ScreenHandler.java
+++ src/screens/ServerLogs/ScreenHandler.java
@@ -2,6 +2,8 @@ package src.screens.ServerLogs;
 
 import java.util.Date;
 
+import javax.swing.Box;
+
 import src.components.UserConversationItem;
 import src.components.UserMessageItem;
 import src.entities.Client;
@@ -20,11 +22,11 @@ public class ScreenHandler {
             @Override
             public void execute(Client member) {
                 try {
-                    System.out.println("MEMBER JOINED " + member.getName());
                     UserConversationItem memberItem = new UserConversationItem(member);
                     ClientMessage clientMessage = new ClientMessage(member, "Se conectou", new Date());
 
                     screen.getLogsPanel().add(new UserMessageItem(clientMessage)).revalidate();
+                    screen.getMembersPanel().add(Box.createVerticalStrut(10)).revalidate();
                     screen.getMembersPanel().add(memberItem).revalidate();
                 } catch (Exception e) {
                     System.out.println(e);
