338a093db3f29c67dc361bcd8297e6b767ac7855
Felipe Dasr <felipe.heiker07@gmail.com>
2023-06-07 20:27:08 -0300

    refactor> remove usused attribute

--- src/components/ErrorMessage.java
+++ src/components/ErrorMessage.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.io.IOException;
-import java.lang.reflect.Constructor;
 
 import javax.swing.JLabel;
 import javax.swing.JTextField;
