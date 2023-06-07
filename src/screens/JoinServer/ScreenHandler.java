b9563f000037185fe30dc55c167e664626db1ad3
Felipe Dasr <felipe.heiker07@gmail.com>
2023-06-07 20:47:19 -0300

    feat> initial structure of joinServerOnClick

--- src/screens/JoinServer/ScreenHandler.java
+++ src/screens/JoinServer/ScreenHandler.java
@@ -1,10 +1,22 @@
 package src.screens.JoinServer;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 // private class (only to be used by JoinServerScreen.java)
 class ScreenHandler {
     private final JoinServerScreen frame;
 
     public ScreenHandler(JoinServerScreen frame) {
         this.frame = frame;
+    }
+
+    public ActionListener joinServerOnClick() {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
     }
 }
